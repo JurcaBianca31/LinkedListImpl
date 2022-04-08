411a2257051cfd932ba85359987b3fa5da55dfb1
BiancaJurca <bianca.jurca1@gmail.com>
2022-04-08 11:48:58 +0300

    PawnShop

--- DataStructures/src/PawnShop/Jewellery.java
+++ DataStructures/src/PawnShop/Jewellery.java
@@ -6,19 +6,6 @@ public class Jewellery extends Produce {
         this.name = name;
     }
 
-    public double value(double price){
-        this.price = price;
-        return price;
-    }
 
-    public double offer(double offer){
-        this.offer = offer;
-        return offer;
-    }
-
-    public void detail(String detail){
-        this.detail = detail;
-        System.out.println(detail);
-    }
 
 }
--- DataStructures/src/PawnShop/Produce.java
+++ DataStructures/src/PawnShop/Produce.java
@@ -2,9 +2,24 @@ package PawnShop;
 
 public class Produce {
 
-    public String detail;
     public String name;
+    public String detail;
     public double offer;
     public double price;
+
+    public double value(double price){
+        this.price = price;
+        return price;
+    }
+
+    public double offer(double offer){
+        this.offer = offer;
+        return offer;
+    }
+
+    public void detail(String detail){
+        this.detail = detail;
+        System.out.println(detail);
+    }
 
 }
